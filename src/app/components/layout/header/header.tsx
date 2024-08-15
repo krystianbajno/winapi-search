669efc03cff066b88cf71c259cef72f5c311ca5a
Krystian Bajno <krystian.bajno@gmail.com>
2024-08-15 11:57:07 +0200

    imports

--- src/app/components/layout/header/header.tsx
+++ src/app/components/layout/header/header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.headerLeft}>
-        <h1 className={styles.logo}>WinAPI <span className={styles.search}>Search</span></h1>
+        <h1 className={styles.logo}><a href="/">WinAPI <span className={styles.search}>Search</span></a></h1>
       </div>
       <div className={styles.headerCenter}>
         <input
