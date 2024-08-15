08d546339b249951cc1bc65fe2e2e254cde7ed8e
Krystian Bajno <krystian.bajno@gmail.com>
2024-08-15 11:38:01 +0200

    useful links

---
+++ src/app/components/useful-links/useful-links.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from "./useful-links.module.scss";
+
+interface UsefulLinksProps {
+  links: { name: string; href: string }[];
+}
+
+const UsefulLinks: React.FC<UsefulLinksProps> = ({ links }) => {
+  return (
+    <div className={styles.usefulLinks}>
+      {links.map((link, index) => (
+        <a className={styles.link} key={`${link}-${index}`} href={link.href}>
+          - {link.name}
+        </a>
+      ))}
+    </div>
+  );
+};
+
+export default UsefulLinks;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,17 @@ import styles from "./page.module.scss";
 import WinApiSearch from "./components/winapi-search/winapi-search";
 import { useState } from 'react';
 import ToggleDllContentsSwitch from './components/controls/toggle-dll-contents-switch';
+import UsefulLinks from "./components/useful-links/useful-links";
 
+
+const links = [
+  {name: "Microsoft WinAPI Documentation", href: "https://learn.microsoft.com/en-us/windows/win32/api/"},
+  {name: "PHNT", href: "https://github.com/winsiderss/phnt/tree/master"},
+  {name: "Undocumented NTInternals", href: "http://undocumented.ntinternals.net/"},
+  {name: "ReactOS Documentation", href: "https://doxygen.reactos.org/d6/d9e/include_2reactos_2wine_2winternl_8h.html#a105971fca93ccfeff87707348ea92ac3"},
+  {name: "MalAPI.io", href: "https://malapi.io/#"},
+  {name: "Unprotect.it", href: "https://unprotect.it"}
+]
 
 export default function Home() {
   const [showContents, setShowContents] = useState(true);
@@ -16,6 +26,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <section className={styles.pageSection} id="winapi-search">
+        <UsefulLinks links={links}/>
         <ToggleDllContentsSwitch isChecked={showContents} onToggle={handleToggle}/>
         <WinApiSearch showContents={showContents} />
       </section>
