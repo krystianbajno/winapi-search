64733334f2a9fe25ec1a000325e90245b3e7ec14
Krystian Bajno <krystian.bajno@gmail.com>
2024-08-19 22:40:08 +0200

    further optimizations

--- src/app/components/dll/dll.tsx
+++ src/app/components/dll/dll.tsx
@@ -2,14 +2,18 @@ import React, { useState, useEffect, Suspense } from 'react';
 import { IWinApiDll } from '@/app/interfaces/winapi-dll';
 import Function from '@/app/components/function/function';
 import styles from "@/app/components/dll/dll.module.scss";
+import { useInfiniteScroll } from '@/app/hooks/use-infinite-scroll';
 
 interface DllProps {
   dll: IWinApiDll;
   showContents: boolean;
 }
 
+const ITEMS_PER_PAGE = 32;
 const Dll: React.FC<DllProps> = ({ dll, showContents }) => {
   const [isContentVisible, setIsContentVisible] = useState(showContents);
+
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     setIsContentVisible(showContents);
@@ -18,6 +22,11 @@ const Dll: React.FC<DllProps> = ({ dll, showContents }) => {
   const toggleContents = () => {
     setIsContentVisible(prevState => !prevState);
   };
+
+  const observerRef = useInfiniteScroll({
+    onIntersect: () => setPage(prevPage => prevPage + 1),
+  });
+
 
   return (
     <div className={styles.dllSection}>
@@ -30,7 +39,8 @@ const Dll: React.FC<DllProps> = ({ dll, showContents }) => {
             <Suspense fallback={<div>Loading...</div>}>
               <Function key={`${fn.function_name}-${index}`} fn={fn} />
             </Suspense>
-          ))}
+          )).slice(0, page * ITEMS_PER_PAGE)}
+          <div ref={observerRef} style={{ height: '20px', background: 'transparent' }} />
         </ul>
       )}
     </div>
