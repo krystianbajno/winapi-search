603c87230c42873631df53fba9174b8fed0afc76
Krystian Bajno <krystian.bajno@gmail.com>
2024-08-20 13:25:50 +0200

    wapis

---
+++ src/app/api/ms/route.ts
@@ -0,0 +1,10 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  try {
+    return Response.redirect("https://github.com/microsoft/windows-rs/tree/master/crates/libs/bindgen/default")
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return NextResponse.json({ error: 'Failed to fetch data from external API' }, { status: 500 });
+  }
+}
